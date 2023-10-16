3605704163baae8e788680765f06bdd01d8d85a8
trungchanh2002 <trungchanh.kda@gmail.com>
2023-10-16 23:22:47 +0700

    Fix all errors

--- App.js
+++ App.js
@@ -1,143 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image, Button } from "react-native";
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Screen2 from "./Screen2";
+import Home from "./Home";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.body1}>
-        <Image source={require("./assets/vs_blue.png")} style={styles.body1} />
-      </View>
-      <View style={styles.body2}>
-        <Text style={styles.tieude1}>
-          Điện Thoại Vsmart Joy 3 - Hàng chính hãng
-        </Text>
-        <View style={styles.starContainer}>
-          <Image source={require("./assets/sao1.png")} style={styles.star} />
-          <Image source={require("./assets/sao1.png")} style={styles.star} />
-          <Image source={require("./assets/sao1.png")} style={styles.star} />
-          <Image source={require("./assets/sao1.png")} style={styles.star} />
-          <Image source={require("./assets/sao1.png")} style={styles.star} />
-          <Text style={styles.tieude2}>(Xem 828 đánh giá)</Text>
-        </View>
-        <View style={styles.price}>
-          <Text style={styles.tieude3}>1.790.000 đ</Text>
-          <Text style={styles.tieude4}>1.790.000 đ</Text>
-        </View>
-        <View style={styles.refund}>
-          <Text style={styles.tieude5}>Ở ĐÂU RẺ HƠN HOÀN TIỀN</Text>
-          <Image source={require("./assets/group.png")} style={styles.group} />
-        </View>
-      </View>
-      <View style={styles.body3}>
-        <Text style={styles.btnchonmau}>4 MÀU - CHỌN MÀU </Text>
-        <Text style={styles.abc}></Text>
-      </View>
-      <View style={styles.body4}>
-        <Text style={styles.btnchonmua}>Chọn mua</Text>
-      </View>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Screen2" component={Screen2} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingLeft: 20,
-    paddingRight: 20,
-  },
-  body1: {
-    flex: 4.5,
-    width: 300,
-  },
-  body2: {
-    flex: 2,
-  },
-  body3: {
-    flex: 1.5,
-  },
-  body4: {
-    flex: 1,
-  },
-  tieude1: {
-    fontSize: 18,
-  },
-  star: {
-    width: 30,
-    height: 30,
-    marginRight: 5,
-  },
-  starContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  tieude2: {
-    fontSize: 18,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  price: {
-    paddingTop: 10,
-    flexDirection: "row",
-    alignItems: "center",
-    // justifyContent: "space-between",
-  },
-  tieude3: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginRight: 35,
-  },
-  tieude4: {
-    fontSize: 17,
-    fontWeight: "bold",
-    textDecorationLine: "line-through",
-    color: "#444444",
-  },
-  refund: {
-    paddingTop: 10,
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  tieude5: {
-    fontSize: 15,
-    fontWeight: "bold",
-    color: "red",
-  },
-  group: {
-    width: 25,
-    height: 25,
-    marginLeft: 5,
-  },
-  btn1: {
-    backgroundColor: "#3498db",
-    padding: 10,
-    borderRadius: 20,
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  btnchonmau: {
-    fontSize: 18,
-    borderRadius: 10,
-    textAlign: "center",
-    fontWeight: "bold",
-    paddingTop: 15,
-    width: 350,
-    height:55,
-    borderWidth: 2,
-    borderColor: "black",
-  },
-  btnchonmua: {
-    color: "white",
-    fontSize: 20,
-    borderRadius: 10,
-    textAlign: "center",
-    fontWeight: "bold",
-    backgroundColor: "red",
-    padding: 12,
-    width: 350,
-    
-  },
-});
---
+++ Screen2.js
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+const Screen2 = () => {
+  const navigation = useNavigation();
+  const [imageSource, setImageSource] = useState(
+    require("./assets/vs_blue.png")
+  );
+
+  const imageMap = {
+    blue: require("./assets/vs_blue.png"),
+    red: require("./assets/vs_red.png"),
+    black: require("./assets/vs_black.png"),
+    silver: require("./assets/vs_silver.png"),
+  };
+
+  const handlePress1 = () => {
+    setImageSource(imageMap.black);
+  };
+  const handlePress2 = () => {
+    setImageSource(imageMap.blue);
+  };
+  const handlePress3 = () => {
+    setImageSource(imageMap.red);
+  };
+  const handlePress4 = () => {
+    setImageSource(imageMap.silver);
+  };
+  const handlePress5 = () => {
+    navigation.navigate("Home", { selectedImageSource: imageSource });
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.body}>
+        <Image source={imageSource} style={styles.imagevs} />
+        <Text style={styles.text1}>
+          Điện Thoại Vsmart Joy 3 Hàng chính hãng
+        </Text>
+      </View>
+
+      <View style={styles.body2}>
+        <Text style={styles.text2}>Chọn một màu bên dưới:</Text>
+        <TouchableOpacity onPress={handlePress1} style={styles.buttonContainer}>
+          <Text
+            style={[styles.btnchonmau, { backgroundColor: "black" }]}
+          ></Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handlePress2} style={styles.buttonContainer}>
+          <Text
+            style={[styles.btnchonmau, { backgroundColor: "#0066FF" }]}
+          ></Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handlePress3} style={styles.buttonContainer}>
+          <Text style={[styles.btnchonmau, { backgroundColor: "red" }]}></Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handlePress4} style={styles.buttonContainer}>
+          <Text
+            style={[styles.btnchonmau, { backgroundColor: "#FFFFCC" }]}
+          ></Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.body3}>
+        <TouchableOpacity onPress={handlePress5}>
+          <Text style={[styles.btnxong, { backgroundColor: "#0033CC" }]}>
+            XONG
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  body: {
+    flex: 2,
+    flexDirection: "row",
+  },
+  body2: {
+    flex: 6,
+    backgroundColor: "#CCCCCC",
+    width: "100%",
+  },
+  body3: {
+    flex: 1,
+    backgroundColor: "#CCCCCC",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text1: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  text2: {
+    textAlign: "left",
+  },
+  imagevs: {
+    width: 100,
+    height: 120,
+  },
+  btnchonmau: {
+    borderRadius: 5,
+    width: 90,
+    height: 90,
+    borderWidth: 2,
+    borderColor: "black",
+  },
+  btnxong: {
+    borderRadius: 5,
+    borderWidth: 2,
+    width: 300,
+    height: 50,
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    lineHeight: 50,
+    color: "#ffffff",
+  },
+  buttonContainer: {
+    margin: 2,
+    alignItems: "center",
+  },
+});
+
+export default Screen2;
